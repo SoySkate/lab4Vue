82f073c808f831d22281d96dfc14fe3c1bfabdbd
Andrew <andreumartintorres.amt@gmail.com>
2022-10-04 17:35:00 +0200

    wikiwtf

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary">
+    <div class="container">
+      <!-- <RouterLink class="navbar-brand" to="/">Wiki Countries</RouterLink> -->
+      <a class="navbar-brand" href="/">Wiki Countries</a>
+      <a class="text-white" href="https://www.google.com/">Built by Andrew</a>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style></style>
